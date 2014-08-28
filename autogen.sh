df780c8366d16cb7601672ecd690fc0896b5c65d
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-08-28 15:39:51 +0200

    Use  and  if set.

--- autogen.sh
+++ autogen.sh
@@ -15,4 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-autoreconf --install "$@" || exit 1
+: ${AUTORECONF=autoreconf}
+AUTORECONFFLAGS="--verbose --install --force $ACLOCAL_FLAGS"
+
+${AUTORECONF} ${AUTORECONFFLAGS} "$@" || exit 1
