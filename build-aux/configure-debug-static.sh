275f20dc3b374ca2438a878ae8e8950e55cb95bd
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-09-09 18:12:24 +0200

    Add Emacs local variables to shell script files.

--- build-aux/configure-debug-static.sh
+++ build-aux/configure-debug-static.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# -*- coding: utf-8; mode: sh; indent-tabs-mode: nil; tab-width: 2; sh-basic-offset: 2; sh-indentation: 2; fill-column: 75; -*-
 
 ../../configure --disable-shared CPPFLAGS="-DDEBUG" CXXFLAGS="-g -O0"
--- build-aux/git-generate-revision.sh
+++ build-aux/git-generate-revision.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# -*- coding: utf-8; mode: sh; indent-tabs-mode: nil; tab-width: 2; sh-basic-offset: 2; sh-indentation: 2; fill-column: 75; -*-
 
 REVISION_COUNT=$(git log --oneline | wc -l)
 
