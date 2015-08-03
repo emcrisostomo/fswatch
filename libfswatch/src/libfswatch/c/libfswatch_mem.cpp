6b3b01c3399508e915f864002728974a1cfabafc
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2015-08-03 18:35:51 +0200

    Add memory management functions.

---
+++ libfswatch/src/libfswatch/c/libfswatch_mem.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015 Enrico M. Crisostomo
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifdef HAVE_CONFIG_H
+#  include "libfswatch_config.h"
+#endif
+#include "libfswatch_mem.h"
+#include <cstdlib>
+
+void * fsw_alloc(size_t size)
+{
+  return ::malloc(size);
+}
+
+void fsw_free(void * ptr)
+{
+  ::free(ptr);
+}
+
