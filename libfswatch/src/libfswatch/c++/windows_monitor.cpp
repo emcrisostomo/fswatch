0af8aad5d3f3710dccbcdceeb2f8a3a08adf3699
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2015-08-19 00:59:13 +0200

    Create RAII wrapper for HANDLE.

--- libfswatch/src/libfswatch/c++/windows_monitor.cpp
+++ libfswatch/src/libfswatch/c++/windows_monitor.cpp
@@ -43,18 +43,83 @@ using namespace std;
 
 namespace fsw
 {
+  REGISTER_MONITOR_IMPL(windows_monitor, windows_monitor_type);
+
+  class CHandle
+  {
+  public:
+    CHandle() : h(INVALID_HANDLE_VALUE){}
+    CHandle(HANDLE handle) : h(handle){}
+
+    ~CHandle()
+    {
+      if (h != INVALID_HANDLE_VALUE)
+      {
+        CloseHandle(h);
+      }
+    }
+
+    operator HANDLE() const { return h; }
+
+    bool is_valid() const
+    {
+      return (h != INVALID_HANDLE_VALUE);
+    }
+
+    CHandle(const CHandle&) = delete;
+    CHandle& operator=(const CHandle&) = delete;
+
+    CHandle(CHandle&& other)
+    {
+      h = other.h;
+
+      other.h = INVALID_HANDLE_VALUE;
+    }
+
+    CHandle& operator=(CHandle&& other)
+    {
+      if (this == &other) return *this;
+
+      if (h != INVALID_HANDLE_VALUE) CloseHandle(h);
+
+      h = other.h;
+      other.h = INVALID_HANDLE_VALUE;
+
+      return *this;
+    }
+
+  private:
+    HANDLE h;
+  };
+
+  typedef struct DirectoryChangeEvent
+  {
+    CHandle handle;
+    DWORD nBufferLength;
+    size_t bufferSize;
+    DWORD bytesReturned;
+    unique_ptr<void, decltype(::free)*> lpBuffer = {nullptr, ::free};
+    OVERLAPPED overlapped;
+
+    DirectoryChangeEvent() : handle(INVALID_HANDLE_VALUE), nBufferLength(16),
+                             bufferSize(sizeof(FILE_NOTIFY_INFORMATION) * nBufferLength),
+                             bytesReturned(), overlapped()
+    {
+      lpBuffer.reset(::malloc(bufferSize));
+      if (lpBuffer.get() == nullptr) throw libfsw_exception(_("::malloc failed."));
+    }
+  } DirectoryChangeEvent;
+
   struct windows_monitor_load
   {
     fsw_hash_set<wstring> win_paths;
-
+    fsw_hash_map<wstring, DirectoryChangeEvent> dce_by_path;
     fsw_hash_map<wstring, int> descriptors_by_file_name;
     fsw_hash_map<int, string> file_names_by_descriptor;
     fsw_hash_set<int> descriptors_to_remove;
     fsw_hash_set<int> descriptors_to_rescan;
     fsw_hash_map<int, mode_t> file_modes;
   };
-
-  REGISTER_MONITOR_IMPL(windows_monitor, windows_monitor_type);
 
   windows_monitor::windows_monitor(vector<string> paths_to_monitor,
                                  FSW_EVENT_CALLBACK * callback,
@@ -86,44 +151,44 @@ namespace fsw
     }
   }
 
-
-  typedef struct DirectoryChangeEvent
-  {
-    HANDLE handle;
-    DWORD nBufferLength;
-    size_t bufferSize;
-    DWORD bytesReturned;
-    unique_ptr<void, decltype(::free)*> lpBuffer = {nullptr, ::free};
-    OVERLAPPED overlapped;
-
-    DirectoryChangeEvent() : handle(), nBufferLength(16),
-                             bufferSize(sizeof(FILE_NOTIFY_INFORMATION) * nBufferLength),
-                             bytesReturned(), overlapped()
-    {
-      lpBuffer.reset(::malloc(bufferSize));
-      if (lpBuffer.get() == nullptr) throw libfsw_exception(_("::malloc failed."));
-    }
-  } DirectoryChangeEvent;
-
   void windows_monitor::run()
   {
     initialize_windows_path_list();
 
-    DirectoryChangeEvent dce;
+    // DirectoryChangeEvent target;
+    {
+      DirectoryChangeEvent dcee;
+      // dcee.handle = std::move(CHandle((HANDLE)22));
+
+      // if (dce.lpBuffer == nullptr) cout << "Source null!" << endl;
+      // else cout << "Source not null!" << endl;
+
+      // if (target.lpBuffer == nullptr) cout << "Target null!" << endl;
+      // else cout << "Target not null!" << endl;
+
+      load->dce_by_path[L"aaa"] = std::move(dcee);
+      cout << "Moving DCE finished." << endl;
+    }
+
+    DirectoryChangeEvent & dce = load->dce_by_path[L"aaa"];
 
     while (true)
     {
       for (const auto & path : load->win_paths)
       {
-        dce.handle = CreateFileW(path.c_str(),
-                                 GENERIC_READ,
-                                 FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
-                                 nullptr, OPEN_EXISTING,
-                                 FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,
-                                 nullptr);
+        HANDLE h = CreateFileW(path.c_str(),
+                               GENERIC_READ,
+                               FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
+                               nullptr, OPEN_EXISTING,
+                               FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED,
+                               nullptr);
+        dce.handle = h;
 
-        if (dce.handle == INVALID_HANDLE_VALUE)
+        cout << "Create file handle: " << (HANDLE)dce.handle << endl;
+
+        if (!dce.handle.is_valid())
         {
+          // To do: format error message
           wcerr << L"Invalid handle when opening " << path << endl;
           continue;
         }
@@ -132,60 +197,60 @@ namespace fsw
                                             TRUE,
                                             FALSE,
                                             nullptr);
+
         if (dce.overlapped.hEvent == NULL) throw libfsw_exception(_("CreateEvent failed."));
 
         while (true)
         {
-        BOOL b = ReadDirectoryChangesW(dce.handle,
-                                       dce.lpBuffer.get(),
-                                       dce.bufferSize,
-                                       TRUE,
-                                       FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_DIR_NAME |
-                                       FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_LAST_ACCESS | FILE_NOTIFY_CHANGE_CREATION,
-                                       &dce.bytesReturned,
-                                       &dce.overlapped,
-                                       nullptr);
-
-        if (!b) throw libfsw_exception(_("ReadDirectoryChangesW failed."));
-
-        BOOL res = GetOverlappedResult(dce.handle, &dce.overlapped, &dce.bytesReturned, TRUE);
-        if (res == FALSE || dce.bytesReturned == 0)
-        {
-          LPWSTR pTemp = nullptr;
-          DWORD retSize = FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ARGUMENT_ARRAY,
-                                         NULL,
-                                         GetLastError(),
-                                         LANG_NEUTRAL,
-                                         (LPWSTR)&pTemp,
-                                         0,
+          BOOL b = ReadDirectoryChangesW((HANDLE)dce.handle,
+                                         dce.lpBuffer.get(),
+                                         dce.bufferSize,
+                                         TRUE,
+                                         FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_DIR_NAME |
+                                         FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_LAST_ACCESS | FILE_NOTIFY_CHANGE_CREATION,
+                                         &dce.bytesReturned,
+                                         &dce.overlapped,
                                          nullptr);
 
-          if (retSize > 0)
-          {
-            wcerr << pTemp << endl;
-            LocalFree(pTemp);
-          }
-          else
-          {
-            cerr << "Cannot format system error message." << endl;
-          }
-        }
+          if (!b) throw libfsw_exception(_("ReadDirectoryChangesW failed."));
 
-        FILE_NOTIFY_INFORMATION * currEntry = static_cast<FILE_NOTIFY_INFORMATION *>(dce.lpBuffer.get());
-
-        while (currEntry != nullptr)
-        {
-          cout << "Next entry offset " << currEntry->NextEntryOffset << endl;
-          if (currEntry->FileNameLength > 0)
+          BOOL res = GetOverlappedResult(dce.handle, &dce.overlapped, &dce.bytesReturned, TRUE);
+          if (res == FALSE || dce.bytesReturned == 0)
           {
-            wchar_t * stringBuffer = static_cast<wchar_t *>(LocalAlloc(LMEM_ZEROINIT, currEntry->FileNameLength + sizeof(wchar_t)));
-            if (stringBuffer == nullptr) throw libfsw_exception(_("::LocalAlloc failed."));
-            memcpy(stringBuffer, currEntry->FileName, currEntry->FileNameLength);
-            wcout << stringBuffer << endl;
-            LocalFree(stringBuffer);
+            LPWSTR pTemp = nullptr;
+            DWORD retSize = FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                           NULL,
+                                           GetLastError(),
+                                           0,
+                                           (LPWSTR)&pTemp,
+                                           0,
+                                           nullptr);
+
+            if (retSize > 0)
+            {
+              wcerr << pTemp << endl;
+              LocalFree(pTemp);
+            }
+            else
+            {
+              cerr << "Cannot format system error message." << endl;
+            }
           }
-          currEntry = (currEntry->NextEntryOffset == 0) ? nullptr : currEntry + currEntry->NextEntryOffset;
-        }
+
+          FILE_NOTIFY_INFORMATION * currEntry = static_cast<FILE_NOTIFY_INFORMATION *>(dce.lpBuffer.get());
+
+          while (currEntry != nullptr)
+          {
+            if (currEntry->FileNameLength > 0)
+            {
+              wchar_t * stringBuffer = static_cast<wchar_t *>(LocalAlloc(LMEM_ZEROINIT, currEntry->FileNameLength + sizeof(wchar_t)));
+              if (stringBuffer == nullptr) throw libfsw_exception(_("::LocalAlloc failed."));
+              memcpy(stringBuffer, currEntry->FileName, currEntry->FileNameLength);
+              wcout << stringBuffer << endl;
+              LocalFree(stringBuffer);
+            }
+            currEntry = (currEntry->NextEntryOffset == 0) ? nullptr : currEntry + currEntry->NextEntryOffset;
+          }
         }
       }
     }
