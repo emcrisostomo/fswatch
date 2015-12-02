d52a97fd835bb65207731eb6611dc68d16bfd331
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2015-12-02 18:34:48 +0100

    Add README.cmake.

---
+++ README.cmake
@@ -0,0 +1,7 @@
+README.cmake
+============
+
+The CMake files included in the fswatch source distribution are provided as-is,
+in the hope that they will be useful, but **are not** guaranteed to work.  No
+bug report related to these files will be considered by the authors.
+
