d1567731f2cb9db63e1e16627be812c7263657af
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2016-01-10 18:40:40 +0100

    Generate Doxygen documentation from libfswatch_log.h.

--- libfswatch/src/libfswatch/c/libfswatch_log.h
+++ libfswatch/src/libfswatch/c/libfswatch_log.h
@@ -13,39 +13,49 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/**
+ * @file
+ * @brief Header of the `libfswatch` library containing logging functions..
+ *
+ * @copyright Copyright (c) 2014-2015 Enrico M. Crisostomo
+ * @license GNU General Public License v. 3.0
+ * @author Enrico M. Crisostomo
+ * @version 1.8.0
+ */
+
 #ifndef LIBFSW_LOG_H
 #  define LIBFSW_LOG_H
 
 #include <cstdio>
 
-/*
+/**
  * Prints the specified message to standard output.
  */
 void fsw_log(const char * msg);
 
-/*
+/**
  * Prints the specified message to the specified file.
  */
 void fsw_flog(FILE * f, const char * msg);
 
-/*
+/**
  * Formats the specified message and prints it to standard output.  The message
  * string format conforms with printf.
  */
 void fsw_logf(const char * format, ...);
 
-/*
+/**
  * Formats the specified message and prints it to the specified file.  The
  * message string format conforms with printf.
  */
 void fsw_flogf(FILE * f, const char * format, ...);
 
-/*
+/**
  * Prints the specified message using perror.
  */
 void fsw_log_perror(const char * msg);
 
-/*
+/**
  * Prints the specified message using perror.  The message string format
  * conforms with printf.
  */
