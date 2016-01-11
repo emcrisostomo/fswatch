537596ff0e3d635e09f5a60805544619b4f768fe
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2016-01-11 02:41:35 +0100

    Generate Doxygen documentation from filter.hpp.

--- libfswatch/src/libfswatch/c++/filter.hpp
+++ libfswatch/src/libfswatch/c++/filter.hpp
@@ -13,6 +13,18 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/**
+ * @file
+ * @brief Header of the fsw::monitor_filter class.
+ *
+ * This header file defines the fsw::monitor_filter class, a type that
+ * represents a path filter.
+ *
+ * @copyright Copyright (c) 2014-2015 Enrico M. Crisostomo
+ * @license GNU General Public License v. 3.0
+ * @author Enrico M. Crisostomo
+ * @version 1.8.0
+ */
 #ifndef FSW__FILTER_H
 #  define FSW__FILTER_H
 
@@ -21,11 +33,57 @@
 
 namespace fsw
 {
+  /**
+   * @brief Path filters used to accept or reject file change events.
+   *
+   * A path filter is a regular expression used to accept or reject file change
+   * events based on the value of their path.  A filter has the following
+   * characteristics:
+   *
+   *   - It has a _regular expression_ (monitor_filter::text), used to match the
+   *     paths.
+   *
+   *   - It can be an _inclusion_ or an _exclusion_ filter
+   *     (monitor_filter::type).
+   *
+   *   - It can be case _sensitive_ or _insensitive_
+   *     (monitor_filter::case_sensitive).
+   *
+   *   - It can be an _extended_ regular expression (monitor_filter::extended).
+   *
+   * Further information about how filtering works in `libfswatch` can be found
+   * in @ref path-filtering.
+   */
   typedef struct monitor_filter
   {
+    /**
+     * @brief Regular expression used to match the paths.
+     *
+     * Further information about regular expressions can be found here:
+     *
+     * http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap09.html
+     */
     std::string text;
+
+    /**
+     * @brief Filter type.
+     */
     fsw_filter_type type;
+
+    /**
+     * @brief Flag indicating whether monitor_filter::text is a case sensitive
+     * regular expression.
+     */
     bool case_sensitive;
+
+    /**
+     * @brief Flag indicating whether monitor_filter::text is an extended
+     * regular expression.
+     *
+     * Further information about extended regular expressions can be found here:
+     *
+     * http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap09.html#tag_09_04
+     */
     bool extended;
   } monitor_filter;
 }
