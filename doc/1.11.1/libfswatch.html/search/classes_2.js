3277303d9dca5f849116db6c2cf141383fe2dba5
Enrico Maria Crisostomo <enrico.m.crisostomo@gmail.com>
2017-10-27 23:47:07 +0100

    Add documentation for 1.11.1

---
+++ doc/1.11.1/libfswatch.html/search/classes_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['event',['event',['../classfsw_1_1event.html',1,'fsw']]]
+];
---
+++ doc/1.11.1/libfswatch.html/search/functions_a.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['on_5fstop',['on_stop',['../classfsw_1_1fsevents__monitor.html#a19e505e395a2a5dbef427fdcd10f45f8',1,'fsw::fsevents_monitor::on_stop()'],['../classfsw_1_1monitor.html#a7268e813faa0068db46c0aca018594fe',1,'fsw::monitor::on_stop()']]],
+  ['operator_20handle',['operator HANDLE',['../classfsw_1_1win__handle.html#a94213e7de84105d52c82553cfd84e233',1,'fsw::win_handle']]],
+  ['operator_20int',['operator int',['../classfsw_1_1libfsw__exception.html#a4189c5722a6c5e32b0cb97cb4aec2ff2',1,'fsw::libfsw_exception']]],
+  ['operator_3c_3c',['operator&lt;&lt;',['../namespacefsw.html#a4d63cac95aece139196ff84e809e9295',1,'fsw']]],
+  ['operator_3d',['operator=',['../classfsw_1_1monitor.html#a205aeb4a9ad14f4a8cc16aed3e496f16',1,'fsw::monitor::operator=()'],['../classfsw_1_1win__handle.html#a99d4ad2c35bb4b6180d8d4fc5194b890',1,'fsw::win_handle::operator=(const win_handle &amp;)=delete'],['../classfsw_1_1win__handle.html#ac3fc9f8fadc9eff0921c7538b03b81df',1,'fsw::win_handle::operator=(win_handle &amp;&amp;other) noexcept'],['../classfsw_1_1win__handle.html#a957009b1bccd6456c11c6b42b1778d3b',1,'fsw::win_handle::operator=(const HANDLE &amp;handle)']]],
+  ['wstring',['wstring',['../classfsw_1_1win__error__message.html#a2ff8ad64c7c81d9c6238d58cd6f62433',1,'fsw::win_error_message']]]
+];
