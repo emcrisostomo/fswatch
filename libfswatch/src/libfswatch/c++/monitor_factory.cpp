2452f93d0252ebbc7764116bb9c4197b52d7324e
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2018-09-25 14:44:50 +0200

    Fix missing imports

--- libfswatch/src/libfswatch/c++/monitor_factory.cpp
+++ libfswatch/src/libfswatch/c++/monitor_factory.cpp
@@ -13,6 +13,11 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#ifdef HAVE_CONFIG_H
+#  include "libfswatch_config.h"
+#endif
+#include "gettext_defs.h"
+#include "monitor_factory.hpp"
 
 namespace fsw
 {
