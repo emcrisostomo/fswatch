d701526d9e087a49c8e9dbd9b626db078051119e
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2021-06-02 09:55:15 +0200

    Refactor code

--- libfswatch/src/libfswatch/c++/kqueue_monitor.hpp
+++ libfswatch/src/libfswatch/c++/kqueue_monitor.hpp
@@ -83,9 +83,8 @@ namespace fsw
     void rescan_pending();
     void scan_root_paths();
     int wait_for_events(const std::vector<struct kevent>& changes,
-                        std::vector<struct kevent>& event_list);
-    void process_events(const std::vector<struct kevent>& changes,
-                        const std::vector<struct kevent>& event_list,
+                        std::vector<struct kevent>& event_list) const;
+    void process_events(const std::vector<struct kevent>& event_list,
                         int event_num);
 
     int kq = -1;
