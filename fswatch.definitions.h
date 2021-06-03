264b7e37b8ca31f44446211d4b1a32a04ff1dcc2
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2021-06-03 17:35:02 +0200

    Clean up migrated symbols

--- fswatch.definitions.h
+++ fswatch.definitions.h
@@ -1,28 +1,5 @@
-#define HAVE_CXX_UNIQUE_PTR 1 // OK, unused, defined in CM
-#define HAVE_DECL_KEVENT 1 // OK, unused
-#define HAVE_DECL_KQUEUE 1 // OK, unused
-#define HAVE_DLFCN_H 1 // OK, unused
-#define HAVE_FCNTL_H 1 // OK, unused
-#define HAVE_GETOPT_H 1 // OK, unused, defined in CM
-#define HAVE_ICONV 1 // OK, unused
-#define HAVE_INTTYPES_H 1 // OK, unused
-#define HAVE_LIBC 1 // OK, unused
-#define HAVE_LIBPTHREAD 1 // OK, unused
-#define HAVE_MODF 1 // Unused
-#define HAVE_REALPATH 1 // OK, unused, defined in CM
-#define HAVE_SELECT 1 // OK, unused, defined in CM
-#define HAVE_SETLOCALE 1 // OK, unused, defined in CM
-#define HAVE_STDINT_H 1 // OK, unused
-#define HAVE_STDIO_H 1 // OK, unused
-#define HAVE_STDLIB_H 1 // OK, unused
-#define HAVE_STRINGS_H 1 // OK, unused
-#define HAVE_STRING_H 1 // OK, unused
-#define HAVE_SYS_STAT_H 1 // OK, unused, defined in CM
-#define HAVE_SYS_TYPES_H 1 // Unused
-#define HAVE_UNISTD_H 1 // Unused
 #define HAVE_UNORDERED_MAP 1 // OK, used
 #define HAVE_UNORDERED_SET 1 // OK, used
-#define LT_OBJDIR ".libs/" // Unused
 #define MACOS_GE_10_10 1 // OK, used
 #define MACOS_GE_10_5 1 // OK, used
 #define MACOS_GE_10_7 1 // OK, used
