ce6523f0eaa1e1ba51365ccf0b4b6e4044e6e658
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2021-06-03 16:55:40 +0200

    Keep track of defined symbols to migrate them to a CMake configuration file

---
+++ fswatch.definitions.h
@@ -0,0 +1,81 @@
+/* #undef ENABLE_NLS */ // OK, used, defined in CM
+/* #undef HAVE_CYGWIN */ // OK, unused
+/* #undef HAVE_DCGETTEXT */ // OK, unused
+/* #undef HAVE_DECL_CYGWIN_CREATE_PATH */ // OK, unused
+/* #undef HAVE_DECL_FINDCLOSECHANGENOTIFICATION */ // OK, unused
+/* #undef HAVE_DECL_FINDFIRSTCHANGENOTIFICATION */ // OK, unused
+/* #undef HAVE_DECL_FINDNEXTCHANGENOTIFICATION */ // OK, unused
+/* #undef HAVE_DECL_INOTIFY_ADD_WATCH */ // OK, unused
+/* #undef HAVE_DECL_INOTIFY_INIT */ // OK, unused
+/* #undef HAVE_DECL_INOTIFY_RM_WATCH */ // OK, unused
+/* #undef HAVE_DECL_PORT_CREATE */ // OK, unused
+/* #undef HAVE_DECL_READDIRECTORYCHANGESW */ // OK, unused
+/* #undef HAVE_DECL_WAITFORSINGLEOBJECT */ // OK, unused
+/* #undef HAVE_GETTEXT */ // OK, unused
+/* #undef HAVE_PORT_H */ // OK, defined in CM
+/* #undef HAVE_SYS_CYGWIN_H */ // OK, unused
+/* #undef HAVE_SYS_INOTIFY_H */ // OK, defined in CM
+/* #undef HAVE_WINDOWS */ // OK, defined in CM
+/* #undef HAVE_WINDOWS_H */ // OK, unused
+/* #undef HAVE__BOOL */ // OK, unused
+/* #undef LSTAT_FOLLOWS_SLASHED_SYMLINK */ // OK, unused
+/* #undef _UINT32_T */ // OK, unused
+/* #undef mode_t */ // OK
+/* #undef size_t */ // OK
+/* #undef ssize_t */ // OK
+/* #undef uint32_t */ // OK
+#define HAVE_ATEXIT 1 // OK, defined in//  CM
+#define HAVE_CFLOCALECOPYCURRENT 1 // OK, unused
+#define HAVE_CFPREFERENCESCOPYAPPVALUE 1 // OK, unused
+#define HAVE_CORESERVICES_CORESERVICES_H 1 // OK, unused
+#define HAVE_CSTDLIB 1 // OK, unused
+#define HAVE_CXX11 1 // OK, unused
+#define HAVE_CXX_ATOMIC 1 // OK, defined in CM
+#define HAVE_CXX_MUTEX 1 // OK, defined in CM
+#define HAVE_CXX_THREAD_LOCAL 1 // OK, defined in CM
+#define HAVE_CXX_UNIQUE_PTR 1 // OK, defined in CM
+#define HAVE_DECL_KEVENT 1 // OK, unused
+#define HAVE_DECL_KQUEUE 1 // OK, unused
+#define HAVE_DLFCN_H 1 // OK, unused
+#define HAVE_FCNTL_H 1 // OK, unused
+#define HAVE_FSEVENTS_FILE_EVENTS 1 // OK, defined in CM
+#define HAVE_GETOPT_H 1 // OK, defined in CM
+#define HAVE_GETOPT_LONG 1 // OK, defined in CM
+#define HAVE_ICONV 1 // OK, unused
+#define HAVE_INTTYPES_H 1 // OK, unused
+#define HAVE_LIBC 1 // OK, unused
+#define HAVE_LIBPTHREAD 1 // OK, unused
+#define HAVE_MODF 1 // Unused
+#define HAVE_REALPATH 1 // OK, defined in CM
+#define HAVE_SELECT 1 // OK, defined in CM
+#define HAVE_SETLOCALE 1 // OK, defined in CM
+#define HAVE_STDINT_H 1 // OK, unused
+#define HAVE_STDIO_H 1 // OK, unused
+#define HAVE_STDLIB_H 1 // OK, unused
+#define HAVE_STRINGS_H 1 // OK, unused
+#define HAVE_STRING_H 1 // OK, unused
+#define HAVE_STRUCT_STAT_ST_MTIME 1 // OK, used, defined in CM
+#define HAVE_STRUCT_STAT_ST_MTIMESPEC 1 // OK, used, defined in CM
+#define HAVE_SYS_EVENT_H 1 // OK, used, defined in CM
+#define HAVE_SYS_STAT_H 1 // OK, defined in CM
+#define HAVE_SYS_TYPES_H 1 // Unused
+#define HAVE_UNISTD_H 1 // Unused
+#define HAVE_UNORDERED_MAP 1 // OK, used
+#define HAVE_UNORDERED_SET 1 // OK, used
+#define LT_OBJDIR ".libs/" // Unused
+#define MACOS_GE_10_10 1 // OK, used
+#define MACOS_GE_10_5 1 // OK, used
+#define MACOS_GE_10_7 1 // OK, used
+#define MACOS_GE_10_9 1 // OK, used
+
+// metadata
+#define PACKAGE "fswatch"
+#define PACKAGE_BUGREPORT "enrico.m.crisostomo@gmail.com"
+#define PACKAGE_NAME "fswatch"
+#define PACKAGE_STRING "fswatch 1.16.0"
+#define PACKAGE_TARNAME "fswatch"
+#define PACKAGE_URL "https://github.com/emcrisostomo/fswatch"
+#define PACKAGE_VERSION "1.16.0"
+#define STDC_HEADERS 1 // OK, unused
+#define VERSION "1.16.0" // OK, repeated
+
