2abee0341b1468778268db15617d59d40698aa46
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2021-06-06 22:59:49 +0200

    Rename cmake_config.h.in to fswatch_config.h.in

--- fswatch/src/fswatch.cpp
+++ fswatch/src/fswatch.cpp
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "cmake_config.h"
+#include "fswatch_config.h"
 #include "gettext.h"
 #include "fswatch.hpp"
 #include <iostream>
