6a02e4bc0b21c7b27de43a9880c7242ee84c2d70
Enrico Maria Crisostomo <enrico.m.crisostomo@gmail.com>
2025-01-10 16:43:01 +0000

    Update copyright years in cevent.cpp and cevent.h

--- libfswatch/src/libfswatch/c/cevent.cpp
+++ libfswatch/src/libfswatch/c/cevent.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2021 Enrico M. Crisostomo
+ * Copyright (c) 2015-2025 Enrico M. Crisostomo
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
